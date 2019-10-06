3188a50f30d2745c73cdfea68f050615f6503086
hardikgohil73253 <41203440+hardikgohil73253@users.noreply.github.com>
2019-10-06 09:54:17 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>DMS | Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.php" style="font-size: 30px;">Disaster Management system</a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+					
+						<li class="active"><a href="contact.php">contact</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2> Address  :</h2>
+						    	<p>Indian Disaster Management System,</p>
+						    	<p>Indiamarg Road, Rambaug,New Delhi</p>
+						   		<p>India</p>
+				   		<p>Phone:(01) 222 666 444</p>
+				   		<p>Mobile: (+91) 9898780779</p>
+				 	 	<p>Email: <span>info@dms.gov.in</span></p>
+				   	
+				   </div>
+				</div>				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Submit"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   
+		<!--end-wrap-->
+	</body>
+</html>
+
